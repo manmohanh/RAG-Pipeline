c9a0295c98a5fbbec01a950d4a7bdd7e741b12b0
Manmohan Hansda <hmanmohan12@gmail.com>
2026-09-11 13:31:03 +0530

    indexing

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 
 pdf_path = Path(__file__).parent / "test.pdf"
 
@@ -15,3 +17,17 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 chunks = text_splitter.split_documents(documents=docs)
+
+#Vector embeddings
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="learning_rag"
+)
+
+print('indexing of document done.')
